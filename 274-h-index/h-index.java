70a98eb50ee6aa54c79a68da380c47a4778b97d0
Sujal Kumar <142314178+sujalniko@users.noreply.github.com>
2026-09-01 22:42:18 +0530

    Time: 6 ms (29.44%) | Memory: 43.4 MB (73.92%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+      
+        int n = citations.length;
+      
+        for (int h = n; h > 0; h--) {
+
+            if (citations[n - h] >= h) {
+                return h;
+            }
+        }
+      
+        return 0;
+    }
+}
